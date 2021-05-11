e36201cc91c97baa706101dbe560b8f6533ec66a
Alexander Böhm <mheob@users.noreply.github.com>
2021-05-11 13:53:20 +0200

    feat: add a `separateSizeAndUnit` utility function
    This function lives in a new utils package.

---
+++ packages/utils/README.md
@@ -0,0 +1,11 @@
+# @mheob/utils
+
+Shared Vanilla TS/JS utilities for various components and packages.
+
+> This is an internal utility, not intended for public usage.
+
+## Installation
+
+```sh
+yarn add @mheob/utils
+```
---
+++ packages/utils/src/converter.ts
@@ -0,0 +1,19 @@
+/**
+ * Splits a size into its value and the unit. If no unit is given, `px` will returned.
+ * @param size The size which should split
+ * @returns The size as number and the unit
+ */
+export const separateSizeAndUnit = (size: number | string): [number, string] => {
+  let value: number
+  let unit = 'px'
+
+  if (typeof size === 'string') {
+    const fontSizeAsNumberAndUnit = size.split(/[^0-9]+?$/)
+    value = +fontSizeAsNumberAndUnit[0]
+    unit = fontSizeAsNumberAndUnit[1]
+  } else {
+    value = size
+  }
+
+  return [value, unit]
+}
