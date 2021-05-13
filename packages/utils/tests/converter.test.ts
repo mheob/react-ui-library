8de2e2e621aef0ab06f06b283dc332b91c76f0fc
Alexander Böhm <mheob@users.noreply.github.com>
2021-05-13 10:58:57 +0200

    test(utils): add tests to reach a coverage of 100% (#6)

---
+++ packages/utils/tests/converter.test.ts
@@ -0,0 +1,27 @@
+import { separateSizeAndUnit } from '../src'
+
+describe('separateSizeAndUnit Method', () => {
+  it('should return a `size` of `12.5` and a `unit` of `rem` for the given string of `12.5rem`', () => {
+    const [size, unit] = separateSizeAndUnit('12.5rem')
+    expect(size).toBe(12.5)
+    expect(unit).toBe('rem')
+  })
+
+  it('should return a `size` of `12.5` and a `unit` of `px` for the given string of `12.5`', () => {
+    const [size, unit] = separateSizeAndUnit('12.5')
+    expect(size).toBe(12.5)
+    expect(unit).toBe('px')
+  })
+
+  it('should return a `size` of `12.5` and a `unit` of `px` for the given number of `12.5`', () => {
+    const [size, unit] = separateSizeAndUnit(12.5)
+    expect(size).toBe(12.5)
+    expect(unit).toBe('px')
+  })
+
+  it('should return a `size` of `1` and a `unit` of `em` for the given string of `inherit`', () => {
+    const [size, unit] = separateSizeAndUnit('inherit')
+    expect(size).toBe(1)
+    expect(unit).toBe('em')
+  })
+})
