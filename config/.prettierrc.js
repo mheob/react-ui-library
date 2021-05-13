0fa3382bb4f04e2b3742f5838426cde868cc766f
Alexander Böhm <mheob@users.noreply.github.com>
2021-05-13 09:01:00 +0200

    docs: add contributing information and a license file

--- config/.prettierrc.js
+++ config/.prettierrc.js
@@ -1,5 +1,14 @@
 module.exports = {
   printWidth: 100,
+  proseWrap: 'always',
   singleQuote: true,
   semi: false,
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        parser: 'markdown',
+      },
+    },
+  ],
 }
